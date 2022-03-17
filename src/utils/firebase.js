a0a0cda84682049827f6ea926dd6caf4d749aa85
prahalad490 <prhld044@gmail.com>
2022-03-17 22:23:05 +0530

    hide credentials

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,14 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDaLMxqgw3KfO1MgWpTc8Ub2W3EvUSqfis",
-  authDomain: "netfilx-clone-5ba3a.firebaseapp.com",
-  projectId: "netfilx-clone-5ba3a",
-  storageBucket: "netfilx-clone-5ba3a.appspot.com",
-  messagingSenderId: "331926714741",
-  appId: "1:331926714741:web:c29ffba266ea6046c33b3c",
-};
+const firebaseConfig = process.env.REACT_APP_FIREBASE;
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore();
