bcbad3a6aca1491423b44ab913f2c738f4a08cdc
prahalad490 <prhld044@gmail.com>
2022-03-17 22:31:39 +0530

    add firebase credentials

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,7 +3,14 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = process.env.REACT_APP_FIREBASE;
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_apiKey,
+  authDomain: process.env.REACT_APP_FIREBASE_authDomain,
+  projectId: process.env.REACT_APP_FIREBASE_projectId,
+  storageBucket: process.env.REACT_APP_FIREBASE_storageBucket,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_messagingSenderId,
+  appId: process.env.REACT_APP_FIREBASE_appId,
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore();
